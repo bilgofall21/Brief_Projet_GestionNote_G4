4b6d0b9a557b7e3ce8c615abf6ea238c7d656e69
unknown <thiernohabibbah176@gmail.com>
2023-11-14 17:53:32 +0100

    ajout fichier

--- src/app/header-user/header-user.component.ts
+++ src/app/header-user/header-user.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { UserserviceService } from '../serviceuser/userservice.service';
 
 @Component({
   selector: 'app-header-user',
@@ -8,14 +7,5 @@ import { UserserviceService } from '../serviceuser/userservice.service';
 })
 export class HeaderUserComponent {
 
-  constructor(private userService: UserserviceService) {}
-
-  public userId: any = "salut";
-  public nom:any ="salut"
-  // ngOnInit() {
-  //    this.userId = this.userService.getUserId();
-  //   console.log(this)
-  // // Faites ce que vous devez avec l'identifiant récupéré
-  // }
 
 }

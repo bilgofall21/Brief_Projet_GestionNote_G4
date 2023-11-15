27ab85644087cd41ddf742f3a6bdce1afb8899f1
unknown <assanedip999@gmail.com>
2023-11-15 23:55:39 +0000

    first commit

--- src/app/Dashbord_admin/dashbord-classe/dashbord-classe.component.ts
+++ src/app/Dashbord_admin/dashbord-classe/dashbord-classe.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class DashbordClasseComponent {
 
+//   let myArray = [
+//     { id: 1, name: 'Objet 1' },
+//     { id: 2, name: 'Objet 2' },
+//     // ... d'autres objets
+// ];
+
+
+
+  
+  // ajouterContact(){
+    
+  
+  //     // Création de l'objet contact 
+  //     let classe = {
+  //       idClasse this.idLastClasse + 1,
+  //       nomContact: this.nom,
+  //       prenomContact: this.prenom,
+  //       emailContact: this.email,
+  //       telephoneContact: this.telephone,
+  //       descriptionContact: this.description,
+  //       imageContact: this.imageUrl,
+  //       etatContact: 1,
+  //       createAt: new Date(),
+  //       createBy: this.userConnect.email,
+  //       updateAt: "",
+  //       updateBy: "",        
+  //     }
+
+  //     // On ajoute l'objet dans la liste des contacts
+  //     console.log(this.idLastContact);
+  //     this.tabContactsUser.push(contact);
+
+  //     // Ferme le popup si on click sur Ok 
+  //     Swal.fire({
+  //       title: "Felicitation!",
+  //       text: "Contact créé avec succes",
+  //       icon: "success",
+  //     });
+  //     // On vide les champs 
+  //     this.viderChapmsContact();
+  //     // On met à jour le tableau qui est stocké dans le localStorage 
+  //     localStorage.setItem("Users", JSON.stringify(this.tabUsers));
+
+  //     console.log(this.tabContactsUser);
+  //     console.log(this.userConnect);
+  //     console.log(this.tabUsers);
+  //   }
+  // }
+
+
 }
